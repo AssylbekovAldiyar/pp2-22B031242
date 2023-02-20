e94009e61e7e3c2f865736261c2f419b32b4e4f3
AssylbekovAldiyar <123085249+AssylbekovAldiyar@users.noreply.github.com>
2023-02-20 20:02:57 +0600

    Add files via upload

---
+++ lab4/math.py
@@ -0,0 +1,30 @@
+import math
+
+# #1 радиант
+# n = int(input())
+
+# print((n * math.pi)/180)
+
+
+# #2 трапеция
+# h = int(input("высота: "))
+# x = int(input("значие х: "))
+# y = int(input("значение у: "))
+
+# print((x + y)/2 * h)
+
+# #3  площади правильного многоугольника.
+
+# n = int(input("количество сторон: "))
+# a = int(input("длина стороны: "))
+
+# print(int((n*a ** 2)/(4 * math.tan(math.pi/n))))
+
+
+# #4  площади параллелограмма.
+
+# x = int(input())
+# y = int(input())
+# print(x * y)
+
+
